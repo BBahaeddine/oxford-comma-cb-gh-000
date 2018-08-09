d43da4ea39d7131aab601b37fb3fd84e5a09165d
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-09 09:53:21 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,11 @@
 def oxford_comma(array)
   string = ""
   counter = 0
+  if array.size == 1
+     string << "#{array[0]}"
+  elsif array.size == 2
+    string << "#{array[0]} and #{array[1]}"
+  end
   loop do
     init = array.size - 1
     if counter == init
